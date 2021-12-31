83590fccdd0220999f09c8ffe7acc5fc82bd6575
itCarl <mewes.maximilian@gmx.de>
2021-12-31 04:11:43 +0100

    added temporarily impressum

--- resources/views/impressum.blade.php
+++ resources/views/impressum.blade.php
@@ -5,7 +5,18 @@
 @section('content')
 
     <div>
-        <h1>TODO</h1>
+        <h5>Angaben gemäß § 5 TMG </h5>
+        <p>
+            Maximilian Mewes<br>
+            Kleine Mühlenstraße 17<br>
+            14774 Brandenburg an der Havel<br>
+        </p>
+
+        <h5>Angaben gemäß § 5 TMG </h5>
+        <p>
+            Telefon: 01523 3869318<br>
+            E-Mail: mewes.maximilian@gmx.de
+        </p>
     </div>
 
 @endsection
